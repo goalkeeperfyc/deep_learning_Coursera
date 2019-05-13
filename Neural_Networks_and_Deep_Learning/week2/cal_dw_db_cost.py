28ad75967b81dfab710267f8f8ed2b4ef380d832
goalkeeperfyc <664947387@qq.com>
2019-05-13 13:01:09 +0800

    download from coursera

---
+++ Neural_Networks_and_Deep_Learning/week2/cal_dw_db_cost.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  4 02:59:48 2019
+
+@author: fangyucheng
+Email: 664947387@qq.com
+"""
+
+import numpy as np
+
+w = np.array([[1.],[2.]])
+b = 2
+X = np.array([[1.,2.,-1.],[3.,4.,-3.2]])
+Y = np.array([[1,0,1]])
+
+A = 1 / (1 + np.exp(-(w.T.dot(X)+b)))
+print(A)
+print("="*20)
+
+m = X.shape[1]
+print(np.log(A))
+print(np.log(1-A))
+print(Y*np.log(A))
+print((1-Y)*np.log(1-A))
+
+cost = -1/m * np.sum(Y*np.log(A) + (1-Y)*np.log(1-A))
+
+print(cost)
+
+dw = 1/m * X.dot((A-Y).T)
+
+db = 1/m * np.sum(A-Y)
+
+print("dw is " + str(dw))
+print("db is " + str(db))
